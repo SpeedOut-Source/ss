4eadab8ec414d20a8814f898332fbed0f1c34b1f
Biplob Sutradhar <biplobsd11@gmail.com>
2024-05-07 17:44:36 +0600

    switch mode loading

--- components/switch-mode.tsx
+++ components/switch-mode.tsx
@@ -1,17 +1,25 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { LogOut } from "lucide-react";
+import { Loader2, LogOut } from "lucide-react";
 import axios from "axios";
 import { usePathname } from "next/navigation";
-
+import { useState } from "react";
 
 export const SwitchMode = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const pathname = usePathname();
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isCoursePage = pathname?.includes("/courses");
 
   async function switchMode(isTeacher: boolean) {
-    await axios.patch("/api/switchMode", { isTeacher });
+    setIsLoading(true);
+    try {
+      await axios.patch("/api/switchMode", { isTeacher });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   const tExit = async () => switchMode(false);
@@ -19,15 +27,31 @@ export const SwitchMode = () => {
 
   return isTeacherPage || isCoursePage ? (
     <Link href="/" onClick={tExit}>
-      <Button size="sm" variant="ghost">
-        <LogOut className="h-4 w-4 mr-2" />
-        Exit
+      <Button size="sm" variant="ghost" disabled={isLoading}>
+        {isLoading ? (
+          <>
+            {" "}
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+          </>
+        ) : (
+          <>
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </>
+        )}
       </Button>
     </Link>
   ) : (
     <Link href="/teacher/courses" onClick={tEnter}>
-      <Button size="sm" variant="ghost">
-        Teacher mode
+      <Button size="sm" variant="ghost" disabled={isLoading}>
+        {isLoading ? (
+          <>
+            {" "}
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+          </>
+        ) : (
+          "Teacher mode"
+        )}
       </Button>
     </Link>
   );
