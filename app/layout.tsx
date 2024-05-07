8c43a0698373cba38cacc68d51981068e3db1951
Biplob Sutradhar <biplobsd11@gmail.com>
2024-05-07 17:28:43 +0600

    added progressbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastProvider } from "@/components/providers/toaster-provider";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
+import Providers from "@/components/progress-bar-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <ConfettiProvider />
           <ToastProvider />
-          {children}
+          <Providers>{children}</Providers>
         </body>
       </html>
     </ClerkProvider>
