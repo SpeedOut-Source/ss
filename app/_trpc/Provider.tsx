1e293dc121e6d3b0fcdf76767e2b728267759ccb
Zonaid <ahammedzonaid@gmail.com>
2024-06-27 11:59:02 +0600

    ..

--- app/_trpc/Provider.tsx
+++ app/_trpc/Provider.tsx
@@ -6,6 +6,10 @@ import { trpc } from "./client";
 
 // import { trpc } from "./client";
 
+const url = process.env.production
+  ? "https://production.d217dozgmbyzxw.amplifyapp.com/api/trpc"
+  : "http://localhost:3000/api/trpc";
+
 export default function Provider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
