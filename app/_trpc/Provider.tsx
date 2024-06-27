42ca40ba8de1b1177c0b65df030eedc8298f809d
Zonaid <ahammedzonaid@gmail.com>
2024-06-27 11:59:20 +0600

    ..

--- app/_trpc/Provider.tsx
+++ app/_trpc/Provider.tsx
@@ -16,7 +16,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: url,
         }),
       ],
     })
